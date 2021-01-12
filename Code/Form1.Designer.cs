a96202fd6560e204e8f4c76283b02ef674571885
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2021-01-12 11:45:43 +0100

    code

---
+++ Code/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace Gestion_des_clients
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.nom = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(395, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(410, 365);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // nom
+            // 
+            this.nom.AutoSize = true;
+            this.nom.Location = new System.Drawing.Point(46, 115);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(77, 13);
+            this.nom.TabIndex = 4;
+            this.nom.Text = "nom et prénom";
+            this.nom.Click += new System.EventHandler(this.nom_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "adresse";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ville";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Mettre à jour";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "vider";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Créer nouveau";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(216, 299);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 40);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Enregistrer";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(156, 215);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(146, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(156, 65);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(146, 20);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "id";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(117, 368);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 40);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Supprimer";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nom);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label nom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Code/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Gestion_des_clients
+{
+    public partial class Form1 : Form
+    {
+
+        public void Empty(Control f)
+        {
+            foreach (Control ct in f.Controls)
+            {
+                if (ct.GetType() == typeof(TextBox))
+                {
+                    ct.Text = "";
+                }
+                if (ct.Controls.Count != 0)
+                {
+                    Empty(ct);
+                }
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strcn = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Gestionclient;Integrated Security=True";
+        DataSet ds = new DataSet();
+        SqlConnection cn = new SqlConnection();
+        SqlDataAdapter da = new SqlDataAdapter();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //To get data from Sql server
+            cn.ConnectionString = strcn;
+            GetClients();
+
+        }
+
+        void GetClients()
+        {
+            //Creation d'une SQLCommand
+            SqlCommand selectCommand = new SqlCommand("select * from client", cn);
+            da.SelectCommand = selectCommand;
+            da.Fill(ds, "clt");
+            dataGridView1.DataSource = ds.Tables["clt"];
+        }
+
+        private void nom_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("voulez-vous vider les champs ?", "Oui", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Empty(this);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataRow Ligne = ds.Tables["clt"].NewRow();
+            Ligne[0] = int.Parse(textBox4.Text);
+            Ligne[1] = textBox1.Text;
+            Ligne[2] = textBox2.Text;
+            Ligne[3] = textBox3.Text;
+            ds.Tables["clt"].Rows.Add(Ligne);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int p = -1;
+            for (int i = 0; i < ds.Tables["clt"].Rows.Count; i++)
+            {
+                if (textBox4.Text == ds.Tables["clt"].Rows[i][0].ToString())
+                {
+                    p = i;
+                }
+            }
+            if (p == -1) { MessageBox.Show("cilent introvable"); }
+            else
+            {
+                ds.Tables["clt"].Rows[p][0] = int.Parse(textBox4.Text);
+                ds.Tables["clt"].Rows[p][1] = textBox1.Text;
+                ds.Tables["clt"].Rows[p][2] = textBox2.Text;
+                ds.Tables["clt"].Rows[p][3] = textBox3.Text;
+                MessageBox.Show("Le client a été modifié avec succès");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int p = -1;
+            for (int i = 0; i < ds.Tables["clt"].Rows.Count; i++)
+            {
+                if (textBox4.Text == ds.Tables["clt"].Rows[i][0].ToString())
+                {
+                    p = i;
+                }
+            }
+            if (p == -1) { MessageBox.Show("cilent introvable"); }
+            else
+            {
+                ds.Tables["clt"].Rows[p].Delete();
+                MessageBox.Show("Le client a été supprimè avec succès");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+            da.Update(ds, "clt");
+            MessageBox.Show("Les données ont été bien Enregistrées");
+        }
+    }
+}
